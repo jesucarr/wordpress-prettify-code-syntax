61595cc72642fc0f63db9795621a51f56e451118
jesucarr <jesucarr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-15 15:28:15 +0000

    finished adding hooks and filters, frontend working
    git-svn-id: http://plugins.svn.wordpress.org/prettify-code-syntax/trunk@668508 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- prettify-code-syntax.php
+++ prettify-code-syntax.php
@@ -31,11 +31,17 @@ class PrettifyCodeSyntax {
 	function __construct() {
 		$this->plugin_id = 'prettify-code-syntax';
 		$this->plugin_url = plugins_url($this->plugin_id);
+		$this->options = get_option('prettify_code_syntax');
 
   	add_action('admin_menu', array($this, 'menu'));
 		add_action('admin_init', array($this, 'register_settings'));
+		add_action('admin_enqueue_scripts', array($this, 'load_admin_scripts'));
 
-		// add_action('wp_enqueue_scripts', array($this, 'load_scripts'));
+		add_action('wp_enqueue_scripts', array($this, 'load_scripts'));
+		add_action('wp_enqueue_scripts', array($this, 'load_styles'));
+
+		add_filter('the_content', array($this, 'content_filter'));
+		add_filter('comment_text', array($this, 'content_filter'));
 
 		add_action('plugins_loaded', array($this, 'load_plugin_textdomain'));
   }
@@ -72,9 +78,135 @@ class PrettifyCodeSyntax {
 		return $options;
 	}
 
-	function load_scripts() {
-    wp_enqueue_script('prettify', dirname(plugin_basename(__FILE__)).'/javascripts/prettify.js', false, false, true);
+	public function load_scripts() {
+
+    wp_enqueue_script('prettify', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/prettify.js', false, false, true);
+
+    if (!empty($this->options['languages_css'])) {
+    	wp_enqueue_script('prettify-css', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-css.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_sql'])) {
+    	wp_enqueue_script('prettify-sql', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-sql.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_yaml'])) {
+    	wp_enqueue_script('prettify-yaml', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-yaml.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_visual_basic'])) {
+    	wp_enqueue_script('prettify-visual-basic', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-vb.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_clojure'])) {
+    	wp_enqueue_script('prettify-clojure', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-clj.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_scala'])) {
+    	wp_enqueue_script('prettify-scala', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-scala.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_tex'])) {
+    	wp_enqueue_script('prettify-tex', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-tex.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_wikitext'])) {
+    	wp_enqueue_script('prettify-wikitext', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-wiki.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_erlang'])) {
+    	wp_enqueue_script('prettify-erlang', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-erlang.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_go'])) {
+    	wp_enqueue_script('prettify-go', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-go.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_haskell'])) {
+    	wp_enqueue_script('prettify-haskell', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-hs.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_lua'])) {
+    	wp_enqueue_script('prettify-lua', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-lua.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_ocaml'])) {
+    	wp_enqueue_script('prettify-ocaml', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-ml.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_nemerle'])) {
+    	wp_enqueue_script('prettify-nemerle', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-n.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_protocol_buffers'])) {
+    	wp_enqueue_script('prettify-protocol-buffers', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-proto.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_vhdl'])) {
+    	wp_enqueue_script('prettify-vhdl', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-vhdl.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_protocol_buffers'])) {
+    	wp_enqueue_script('prettify-protocol_buffers', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-proto.js', array('prettify'), false, true);
+    }
+    if (!empty($this->options['languages_xquery'])) {
+    	wp_enqueue_script('prettify-xquery', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/lang-xq.js', array('prettify'), false, true);
+    }
+
+    wp_enqueue_script('prettify-load', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/load.js', array('prettify'), false, true);
  	} 
+
+ 	public function load_styles() {
+ 		wp_enqueue_style('prettify', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/stylesheets/prettify.css', false, false, 'all');
+ 		if (!empty($this->options['style'])) {
+ 			switch($this->options['style']) {
+ 				case 'desert':
+	    		wp_enqueue_style('prettify-desert', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/stylesheets/desert.css', 'prettify', false, 'all');
+	    	break;
+	    	case 'sunburst':
+	    		wp_enqueue_style('prettify-sunburst', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/stylesheets/sunburst.css', 'prettify', false, 'all');
+	    	break;
+	    	case 'sons_of_obsidian':
+	    		wp_enqueue_style('prettify-sons-of-obsidian', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/stylesheets/sons-of-obsidian.css', 'prettify', false, 'all');
+	    	break;
+	    	case 'custom':
+	    		wp_enqueue_style('prettify-custom', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/stylesheets/custom.php', 'prettify', false, 'all');
+	    	break;
+	    }
+
+ 		}
+ 	}
+
+ 	public function content_filter($content) {
+
+ 		$prettify_code = false;
+
+ 		$regex = '/(<pre\s+.*?class\s*?=\s*?[",\'].*?prettyprint\b.*?[",\'].*?>)(.*?)(<\/pre>)/si';
+ 		$content = preg_replace_callback($regex, array($this, 'parse_content_pre'), $content);
+ 		$regex = '/(<code\s+[^>]*?class\s*?=\s*?"\s*?prettyprint.*?".*?>)(.*?)(<\/code>)/si';
+ 		// print_r($content);
+ 		$content = preg_replace_callback($regex, array($this, 'parse_content_code'), $content);
+
+ 		return $content;
+ 		
+	}
+
+	public function parse_content_pre($matches) {
+
+		$tags_open = $matches[1];
+		$code = $matches[2];
+		$tags_close = $matches[3];
+
+		$regex = '/(<code.*?>)(.*?)(<\/code>)/si';
+		preg_match($regex, $code, $matches);
+		if(!empty($matches)) {
+			$tags_open .= $matches[1];
+			$code = $matches[2];
+			$tags_close = $matches[3].$tags_close;
+		}
+
+ 		
+ 		$parsed_code = htmlentities($code);
+		return $tags_open.$parsed_code.$tags_close;
+	}
+
+	public function parse_content_code($matches) {
+		// print_r($matches);
+		$tags_open = $matches[1];
+		$code = $matches[2];
+		$tags_close = $matches[3];
+
+		$parsed_code = htmlentities($code);
+		return $tags_open.$parsed_code.$tags_close;
+	}
+
+	public function load_admin_scripts() {
+		wp_enqueue_script('admin-tabs', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/javascripts/admin-tabs.js', array('jquery'), false, true);
+	}
 
 	public function languages_section_description() {
 		include "views/languages-section-description.php";
--- views/style-style-field-content.php
+++ views/style-style-field-content.php
@@ -1,10 +1,9 @@
-<?php $options = get_option('prettify_code_syntax'); ?>
 <fieldset><legend class="screen-reader-text"><span><?php _e('Style', $this->plugin_id) ?></span></legend>
 	<table>
 		<tr>
 			<td>
 				<label for="mh-style-default">
-					<input id="mh-style-default" type="radio" name="prettify_code_syntax[style]" value="default" <?php if ($options['style'] == 'default' || !$options['style']) { echo 'checked="checked"'; } ?> />
+					<input id="mh-style-default" type="radio" name="prettify_code_syntax[style]" value="default" <?php if ($this->options['style'] == 'default' || !$options['style']) { echo 'checked="checked"'; } ?> />
 					<?php _e('Default', $this->plugin_id) ?>
 				</label>
 			</td>
@@ -17,7 +16,7 @@
 		<tr>
 			<td>
 				<label for="mh-style-desert">
-					<input id="mh-style-desert" type="radio" name="prettify_code_syntax[style]" value="desert" <?php if ($options['style'] == 'desert') { echo 'checked="checked"'; } ?> />
+					<input id="mh-style-desert" type="radio" name="prettify_code_syntax[style]" value="desert" <?php if ($this->options['style'] == 'desert') { echo 'checked="checked"'; } ?> />
 					Desert
 				</label>
 			</td>
@@ -30,7 +29,7 @@
 		<tr>
 			<td>
 				<label for="mh-style-sunburst">
-					<input id="mh-style-sunburst" type="radio" name="prettify_code_syntax[style]" value="sunburst" <?php if ($options['style'] == 'sunburst') { echo 'checked="checked"'; } ?> />
+					<input id="mh-style-sunburst" type="radio" name="prettify_code_syntax[style]" value="sunburst" <?php if ($this->options['style'] == 'sunburst') { echo 'checked="checked"'; } ?> />
 					Sunburst
 				</label>
 			</td>
@@ -43,7 +42,7 @@
 		<tr>
 			<td>
 				<label for="mh-style-sons-of-obsidian">
-					<input id="mh-style-sons-of-obsidian" type="radio" name="prettify_code_syntax[style]" value="sons_of_obsidian" <?php if ($options['style'] == 'sons_of_obsidian') { echo 'checked="checked"'; } ?> />
+					<input id="mh-style-sons-of-obsidian" type="radio" name="prettify_code_syntax[style]" value="sons_of_obsidian" <?php if ($this->options['style'] == 'sons_of_obsidian') { echo 'checked="checked"'; } ?> />
 					Sons of Obsidian
 				</label>
 			</td>
@@ -56,7 +55,7 @@
 		<tr>
 			<td>
 				<label for="mh-style-custom">
-					<input id="mh-style-custom" type="radio" name="prettify_code_syntax[style]" value="custom" <?php if ($options['style'] == 'custom') { echo 'checked="checked"'; } ?> />
+					<input id="mh-style-custom" type="radio" name="prettify_code_syntax[style]" value="custom" <?php if ($this->options['style'] == 'custom') { echo 'checked="checked"'; } ?> />
 					<?php _e('Custom', $this->plugin_id) ?>
 				</label>
 			</td>
@@ -64,61 +63,11 @@
 				<label for="mh-style-custom">
 					<textarea cols="90" rows="30" name="prettify_code_syntax[style_custom]">
 <?php 
-if(!empty($options['style_custom'])): 
-echo $options['style_custom']; 
-else: ?>
-/* Based on Sunburst theme */
-
-pre .str, code .str { color: #65B042; } /* string  - green */
-pre .kwd, code .kwd { color: #E28964; } /* keyword - dark pink */
-pre .com, code .com { color: #AEAEAE; font-style: italic; } /* comment - gray */
-pre .typ, code .typ { color: #89bdff; } /* type - light blue */
-pre .lit, code .lit { color: #3387CC; } /* literal - blue */
-pre .pun, code .pun { color: #fff; } /* punctuation - white */
-pre .pln, code .pln { color: #fff; } /* plaintext - white */
-pre .tag, code .tag { color: #89bdff; } /* html/xml tag    - light blue */
-pre .atn, code .atn { color: #bdb76b; } /* html/xml attribute name  - khaki */
-pre .atv, code .atv { color: #65B042; } /* html/xml attribute value - green */
-pre .dec, code .dec { color: #3387CC; } /* decimal - blue */
-
-pre.prettyprint, code.prettyprint {
-	background-color: #000;
-	-moz-border-radius: 8px;
-	-webkit-border-radius: 8px;
-	-o-border-radius: 8px;
-	-ms-border-radius: 8px;
-	-khtml-border-radius: 8px;
-	border-radius: 8px;
+if(!empty($this->options['style_custom'])) {
+	echo $this->options['style_custom']; 
+} else {
+	include(dirname(dirname(__FILE__)).'/stylesheets/custom.css');
 }
-
-pre.prettyprint {
-	width: 95%;
-	margin: 1em auto;
-	padding: 1em;
-	white-space: pre-wrap;
-}
-
-
-/* Specify class=linenums on a pre to get line numbering */
-ol.linenums { margin-top: 0; margin-bottom: 0; color: #AEAEAE; } /* IE indents via margin-left */
-li.L0,li.L1,li.L2,li.L3,li.L5,li.L6,li.L7,li.L8 { list-style-type: none }
-/* Alternate shading for lines */
-li.L1,li.L3,li.L5,li.L7,li.L9 { }
-
-@media print {
-  pre .str, code .str { color: #060; }
-  pre .kwd, code .kwd { color: #006; font-weight: bold; }
-  pre .com, code .com { color: #600; font-style: italic; }
-  pre .typ, code .typ { color: #404; font-weight: bold; }
-  pre .lit, code .lit { color: #044; }
-  pre .pun, code .pun { color: #440; }
-  pre .pln, code .pln { color: #000; }
-  pre .tag, code .tag { color: #006; font-weight: bold; }
-  pre .atn, code .atn { color: #404; }
-  pre .atv, code .atv { color: #060; }
-}
-<?php 
-endif; 
 ?>
 					</textarea>
 				</label>
